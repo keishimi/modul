bfe4e003fba4a349f8ac1b62cd3ac0dea975c080
Alex <alex.safonovaa@yandex.ru>
2024-06-25 15:47:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import fake_math as fm
+import true_math as tm
+#result1 = fm(69, 3)
+result2 = fm(3, 0)
+result3 = tm(49, 7)
+result4 = tm(15, 0)
+
+print(result1)  
+print(result2)  
+print(result3)  
+print(result4)  
